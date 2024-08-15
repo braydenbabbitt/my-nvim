26b1a51ed00715eecf7e03b9da9ba251685d1dcc
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2024-08-15 10:45:57 -0600

    updated telescope config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-`>", function()
-  LazyVim.terminal(nil, { cwd = LazyVim.root() })
+  LazyVim.terminal()
 end, { desc = "Toggle Root Dir Terminal" })
 vim.keymap.set("t", "<C-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })
