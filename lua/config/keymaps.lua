22927abb33eede1cc9ec69c6336a4f387b2bfd05
Brayden Babbitt <brayden@babbitts.net>
2024-08-15 09:18:43 -0600

    added better terminal keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<C-`>", function()
+  LazyVim.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Toggle Root Dir Terminal" })
+vim.keymap.set("t", "<C-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })
