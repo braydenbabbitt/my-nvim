81bbd5f277739f09ccf00006cc7a518e3d24a41d
Brayden Babbitt <brayden@babbitts.net>
2024-08-16 14:21:52 -0600

    fixed some style stuff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,9 +14,12 @@ return {
     opts = {
       defaults = {
         layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
         winblend = 0,
+      },
+      pickers = {
+        buffers = {
+          initial_mode = "normal",
+        },
       },
     },
   },
