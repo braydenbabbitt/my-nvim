4f143b47916641119154b5cb5727229a2e311d91
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2024-08-16 14:04:33 -0600

    updated theme to vscode

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -4,24 +4,57 @@ return {
     lazy = false,
     priority = 1000,
     opts = {},
-    config = function()
-      require("tokyonight").setup({
-        style = "night",
-        styles = {
-          functions = {},
-        },
-        on_colors = function(colors)
-          colors.error = "#eb1717"
-        end,
-      })
-
-      vim.cmd("colorscheme tokyonight")
-    end,
+    -- config = function()
+    --   require("tokyonight").setup({
+    --     style = "night",
+    --     styles = {
+    --       functions = {},
+    --     },
+    --     on_colors = function(colors)
+    --       colors.error = "#eb1717"
+    --     end,
+    --   })
+    --
+    --   vim.cmd("colorscheme tokyonight")
+    -- end,
   },
   {
     "nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
     },
+  },
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+      })
+    end,
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      local c = require("vscode.colors").get_colors()
+      require("vscode").setup({
+        style = "dark",
+        color_overrides = {
+          vscBack = "#0f1012",
+        },
+        group_overrides = {
+          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+          CursorLineNr = { fg = c.vscUiOrange, bg = "NONE" },
+          NvimTreeGitDirty = { fg = c.vscDarkYellow, bg = "NONE" },
+        },
+      })
+
+      vim.cmd("colorscheme vscode")
+    end,
   },
 }
