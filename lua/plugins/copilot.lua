2fb7fd0c01c7e01614e49f0061a449ab657cba09
braydenbabbitt <brayden@babbitts.net>
2024-11-05 13:19:49 -0700

    disabled inline copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,16 +1,19 @@
-return {
-  {
-    "github/copilot.vim",
-    event = "VeryLazy",
-    config = function()
-      vim.keymap.set("i", "<C-Enter>", 'copilot#Accept("<Enter>")', {
-        expr = true,
-        replace_keycodes = false,
-      })
-      vim.g.copilot_no_tab_map = true
-
-      vim.keymap.set("n", "<C-x>", "<Plug>(copilot-dismiss)")
-      vim.keymap.set("i", "<C-x>", "<Plug>(copilot-dismiss)")
-    end,
-  },
-}
+-- return {
+--   {
+--     "github/copilot.vim",
+--     event = "VeryLazy",
+--     opts = {
+--
+--     },
+--     config = function()
+--       vim.keymap.set("i", "<C-Enter>", 'copilot#Accept("<Enter>")', {
+--         expr = true,
+--         replace_keycodes = false,
+--       })
+--       vim.g.copilot_no_tab_map = true
+--
+--       vim.keymap.set("n", "<C-x>", "<Plug>(copilot-dismiss)")
+--       vim.keymap.set("i", "<C-x>", "<Plug>(copilot-dismiss)")
+--     end,
+--   },
+-- }
