b52bbefef2989f2e2096b3a61f31f99cf52d69a8
braydenbabbitt <brayden@babbitts.net>
2025-01-08 13:46:41 -0700

    updated keymaps for lsp and beginning/end of file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,14 @@ vim.keymap.set("n", "<leader>/", "<leader>sG", { desc = "Grep (cwd)", remap = tr
 
 -- Black hole register for 'x' deletion
 vim.keymap.set("n", "x", '"_x')
+
+-- Custom LSP keymaps
+vim.keymap.del("n", "<leader>L")
+vim.keymap.set("n", "<leader>Lr", "<cmd>LspRestart<CR>", { desc = "Restart LSP", remap = true })
+vim.keymap.set("n", "<leader>Li", "<cmd>LspInfo<CR>", { desc = "LSP Info", remap = true })
+
+-- Beginning and end of file keymaps
+vim.keymap.set("n", "G", "G$")
+vim.keymap.set("n", "gg", "gg0")
+vim.keymap.set("v", "G", "G$")
+vim.keymap.set("v", "gg", "gg0")
