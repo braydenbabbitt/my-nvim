5258bbf37712318ccfcbfe90aef4bc928cde1171
braydenbabbitt <1495342+braydenbabbitt@users.noreply.github.com>
2025-03-30 19:06:45 -0600

    init cloak

---
+++ lua/plugins/cloak.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "laytan/cloak.nvim",
+    commit = "648aca6",
+    opts = {
+      enabled = true,
+      cloak_character = "*",
+      cloak_telescope = true,
+      try_all_patterns = true,
+      patterns = {
+        {
+          file_pattern = "*",
+          cloak_pattern = { "brayden%S*(?:babbitt%S*)?" },
+        },
+      },
+    },
+  },
+}
