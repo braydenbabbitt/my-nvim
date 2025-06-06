35513beac147f519782eb71e23c556d64eb3ef98
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2025-06-06 11:26:47 -0600

    improved snacks explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          auto_close = true,
+          git_status_open = false,
+        },
+      },
+    },
+  },
+}
