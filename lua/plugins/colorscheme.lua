29737b1521c9649e8599232e9b90c388fde710ca
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2025-06-10 08:41:40 -0600

    various improvements

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,37 +1,42 @@
 return {
-  -- {
-  --   "Mofiqul/vscode.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     style = "dark",
-  --     color_overrides = {
-  --       vscBack = "#0f1012",
-  --       vscPopupBack = "#0f1012",
-  --     },
-  --   },
-  --   config = function(_, opts)
-  --     local c = require("vscode.colors").get_colors()
-  --     opts.group_overrides = {
-  --       Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-  --       CursorLineNr = { fg = c.vscUiOrange, bg = "NONE" },
-  --     }
-  --     require("vscode").setup(opts)
-  --   end,
-  -- },
-  -- Apply the selected colorscheme
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "dark",
+      color_overrides = {
+        vscBack = "#0f1012",
+        vscPopupBack = "#0f1012",
+      },
+    },
+    config = function(_, opts)
+      local c = require("vscode.colors").get_colors()
+      opts.group_overrides = {
+        Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+        CursorLineNr = { fg = c.vscUiOrange, bg = "NONE" },
+      }
+      require("vscode").setup(opts)
+    end,
+  },
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {
       style = "night",
+      styles = {
+        keywords = {
+          italic = false,
+        },
+      },
     },
   },
+  -- Apply the selected colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "vscode",
     },
   },
 }
