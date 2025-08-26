a69a2c15fcfc757ab1c54a12ce35d9d3c5034790
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2025-08-26 09:44:23 -0600

    improved snippet config

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -9,31 +9,48 @@ return {
       local t = ls.text_node
       local i = ls.insert_node
 
-      for _, ft in ipairs({ "javascript", "typescript", "javascriptreact", "typescriptreact" }) do
-        ls.add_snippets(ft, {
-          s("cc", {
-            t("console.log('brayden-test', "),
-            i(1),
-            t(")"),
-          }),
-        })
+      -- Table of custom snippets by filetype
+      local custom_snippets = {
+        javascript = {
+          s("cc", { t("console.log('brayden-test', "), i(1), t(")") }),
+        },
+        typescript = {
+          s("cc", { t("console.log('brayden-test', "), i(1), t(")") }),
+        },
+        javascriptreact = {
+          s("cc", { t("console.log('brayden-test', "), i(1), t(")") }),
+        },
+        typescriptreact = {
+          s("cc", { t("console.log('brayden-test', "), i(1), t(")") }),
+        },
+        -- Add more filetypes and snippets here
+      }
+
+      -- Register all custom snippets
+      for ft, snippets in pairs(custom_snippets) do
+        ls.add_snippets(ft, snippets)
       end
 
-      vim.keymap.set({ "i", "s" }, "<Tab>", function()
-        if ls.expand_or_jumpable() then
-          ls.expand_or_jump()
-        else
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", true)
-        end
-      end, { silent = true })
+      -- Keybind setup function
+      local function setup_luasnip_keymaps()
+        vim.keymap.set({ "i", "s" }, "<C-CR>", function()
+          if ls.expand_or_jumpable() then
+            ls.expand_or_jump()
+          else
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-CR>", true, false, true), "n", true)
+          end
+        end, { silent = true })
 
-      vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
-        if ls.jumpable(-1) then
-          ls.jump(-1)
-        else
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", true)
-        end
-      end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-S-CR>", function()
+          if ls.jumpable(-1) then
+            ls.jump(-1)
+          else
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-S-CR>", true, false, true), "n", true)
+          end
+        end, { silent = true })
+      end
+
+      setup_luasnip_keymaps()
     end,
   },
 }
